05dc78f3f0c23860edcd36e02202d64e885b552b
Rafael Sá <sanunesrafael@gmail.com>
2022-09-28 12:54:50 -0300

    Project created

---
+++ src/main/java/com/rafael/paymentspringjpa/PaymentSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.rafael.paymentspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSpringJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafael/paymentspringjpa/PaymentSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.paymentspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
